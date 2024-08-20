b47e10bd2705738bd7c4531c2ea0fb9d0235d85e
amirhossein <amirhosseinmobini9@gmail.com>
2024-08-20 17:50:56 +0330

    ✨feature: first handler added

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	server := gin.Default()
+	server.GET("/events", eventsHandler)
+	server.Run(":8080")
+}
+
+func eventsHandler(context *gin.Context) {
+	context.JSON(200, gin.H{"message": "Hello There!"})
 }
